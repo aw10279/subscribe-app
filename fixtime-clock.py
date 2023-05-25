0bdfb30f5929399bb67ebdec473f7339426ec580
aw10279 <aw10279@163.com>
2023-05-25 10:59:51 +0800

    Update fixtime-clock.py

--- fixtime-clock.py
+++ fixtime-clock.py
@@ -1,11 +1,11 @@
 import time
 
-def start_timer(duration):
-    """Starts a timer for the specified duration in minutes."""
+def start_timer():
+    """Starts a 10-minute timer."""
+    duration = 10
     print(f'Starting {duration} minute timer...')
     time.sleep(duration * 60)
     print('Time is up!')
 
 if __name__ == '__main__':
-    duration = int(input('How many minutes do you want to focus for? '))
-    start_timer(duration)
+    start_timer()
